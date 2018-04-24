8ed001f9b37dff8b68c216808c98dc54cf55355a
zhujisheng <30714273+zhujisheng@users.noreply.github.com>
2018-04-24 13:22:10 +0800

    Delete webserver1.py

--- webserver1.py
+++
@@ -1,9 +0,0 @@
-from MiniWebSrv import MiniWebSrv
-
-webserver = MiniWebSrv()
-webserver.start()
-
-while True:
-    client, cliAddr = webserver.accept()
-    if client:
-        srv.handle_client(client, cliAddr)
