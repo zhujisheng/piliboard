c3b971b1945c6c5ab7dbae21a0637e38bcd803aa
zhujisheng <zhujisheng@gmail.com>
2018-06-06 16:14:01 +0800

    irq

--- examples/button_irq.py
+++ examples/button_irq.py
@@ -1,16 +1,22 @@
 from machine import Pin
 
 button = Pin(4, Pin.IN)
+is_pressed = False
 
 def callback1(p):
+    global is_pressed
     button.irq(trigger=Pin.IRQ_RISING, handler=callback2)
-    print("button pressed")
+    if not is_pressed:
+        is_pressed = True
+        print("button pressed")
 
 
 def callback2(p):
+    global is_pressed
     button.irq(trigger=Pin.IRQ_FALLING, handler=callback1)
-    print("button released")
-
+    if is_pressed:
+        is_pressed = False
+        print("button released")
 
 button.irq(trigger=Pin.IRQ_FALLING, handler=callback1)
 
